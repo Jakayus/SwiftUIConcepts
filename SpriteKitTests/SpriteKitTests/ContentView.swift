ee11ce55c0f2c7036f5437706df5702ee96f208a
Joel Sereno <jakayus123@gmail.com>
2022-01-11 23:08:50 -0700

    Test simple "Red Box" Spritekit tutorial
    Simple Spritekit game scene where red boxes fall upon each tap.

--- SpriteKitTests/SpriteKitTests/ContentView.swift
+++ SpriteKitTests/SpriteKitTests/ContentView.swift
@@ -6,11 +6,39 @@
 //
 
 import SwiftUI
+import SpriteKit
+
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        let box = SKSpriteNode(color: SKColor.red, size: CGSize(width: 50, height: 50))
+        box.position = location
+        box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 50, height: 50))
+        addChild(box)
+    }
+}
+
 
 struct ContentView: View {
+    
+    var scene: SKScene {
+        let scene = GameScene()
+        scene.size = CGSize(width: 300, height: 400)
+        scene.scaleMode = .fill
+        return scene
+    }
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        SpriteView(scene: scene)
+            .frame(width: 300, height: 400)
+            .ignoresSafeArea()
     }
 }
 
@@ -19,3 +47,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
