6fff70b855611e42ed705f41144a17e6ef3f4c81
Joel Sereno <jakayus123@gmail.com>
2022-01-11 22:59:54 -0700

    Add new project to the repo

---
+++ SpriteKitTests/SpriteKitTests/SpriteKitTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpriteKitTestsApp.swift
+//  SpriteKitTests
+//
+//  Created by Joel Sereno on 1/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SpriteKitTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
